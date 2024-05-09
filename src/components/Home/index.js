e298523d135515a0e08045a730c6b692f0d61243
Troy <shutman18@gmail.com>
2024-05-09 16:57:04 +1200

    About page + loaders

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -18,6 +19,7 @@ const Home = () => {
       }, [])
 
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -40,6 +42,8 @@ const Home = () => {
             </div>
             <Logo />
         </div>
+        <Loader type='ball-pulse-sync' />
+        </>
     );
 }
 
